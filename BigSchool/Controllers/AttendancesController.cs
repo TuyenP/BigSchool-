13a59900e975beec6934a235e03d0d46f3e66dd8
TUYEN <kknguyen111@gmail.com>
2021-07-11 11:37:07 +0700

    Add project files.

---
+++ BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using BigSchool.DTOs;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+
+namespace BigSchool.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _dbContext = new ApplicationDbContext();
+
+        
+        public IQueryable<Attendance> GetAttendances()
+        {
+            return _dbContext.Attendances;
+        }
+
+        
+        [ResponseType(typeof(Attendance))]
+        public IHttpActionResult GetAttendance(int id)
+        {
+            Attendance attendance = _dbContext.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(attendance);
+        }
+
+       
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAttendance(int id, Attendance attendance)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != attendance.CourseId)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(attendance).State = EntityState.Modified;
+
+            try
+            {
+                _dbContext.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AttendanceExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+       
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+            {
+                return BadRequest("The Attendance already exists!");
+            }
+
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        
+        [ResponseType(typeof(Attendance))]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            Attendance attendance = _dbContext.Attendances.Find(id);
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Attendances.Remove(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok(attendance);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AttendanceExists(int id)
+        {
+            return _dbContext.Attendances.Count(e => e.CourseId == id) > 0;
+        }
+    }
+}
---
+++ BigSchool/Models/Course.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BigSchool.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public bool IsCanceled { get; set; }
+
+        [Required]
+        public String LecturerId { get; set; }
+
+        public ApplicationUser Lecturer { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public String Place { get; set; }
+
+        [Display(Name = "Thời gian")]
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
